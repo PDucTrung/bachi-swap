9d027124ab15fbe21d5b81d5317235907f9a67a4
trung-dev <trungezeal115@gmail.com>
2024-07-07 02:57:48 +0700

    remove user used ref

--- contracts/NodeManager.sol
+++ contracts/NodeManager.sol
@@ -45,7 +45,6 @@ contract NodeManager is Pausable, AccessControl, Ownable {
     mapping(string => AffiliateInformation) private affiliates;
     mapping(address => string) private userAffiliateIdLinks;
     mapping(string => address) private affiliateIdUserLinks;
-    EnumerableSet.AddressSet usersUsedReference;
 
     // Events
     event AddedNode(
@@ -256,7 +255,6 @@ contract NodeManager is Pausable, AccessControl, Ownable {
         // Referral code can only be used once per person
         if (
             affiliateIdUserLinks[affiliateId] != caller &&
-            usersUsedReference.contains(caller) &&
             !affiliates[affiliateId].usersUsed.contains(caller)
         ) {
             address affiliatesOwner = affiliateIdUserLinks[affiliateId];
@@ -266,7 +264,6 @@ contract NodeManager is Pausable, AccessControl, Ownable {
             require(sent, "Failed to send Ether");
             affiliates[affiliateId].totalSales += totalSales;
             affiliates[affiliateId].usersUsed.add(caller);
-            usersUsedReference.add(caller);
         }
 
         nodeContract.safeMint(caller, _nodeId);
@@ -333,18 +330,6 @@ contract NodeManager is Pausable, AccessControl, Ownable {
         returns (uint256)
     {
         return affiliates[affiliateId].usersUsed.length();
-    }
-
-    function userUsedTheReferralCode(address user) public view returns (bool) {
-        return usersUsedReference.contains(user);
-    }
-
-    function getUserUsedTheReferralCodeByIndex(uint256 index)
-        public
-        view
-        returns (address)
-    {
-        return usersUsedReference.at(index);
     }
 
     function getReferenceRevenue() public view returns (uint256) {
